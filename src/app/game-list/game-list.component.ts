4a8ee203b6a6a0df5ede2db157b9cb82859376e3
Paulcait <57989682+Paulcait@users.noreply.github.com>
2020-04-12 16:35:25 +0200

    sfd

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -10,10 +10,10 @@ export class GameListComponent implements OnInit {
   gameList = [
     {name:'Borderlands 3',
     description:'Borderlands 3 è un videogioco action RPG-sparatutto in prima persona sviluppato da Gearbox Software e prodotto da 2K Games. Si tratta del quarto capitolo della serie Borderlands, preceduto da Borderlands: The Pre-Sequel.'},
-    {name:'splatoon 2',
-    description:'Bgydfyufiebfrlands, preceduto da Borderlands: The Pre-Sequel.'},
-    {name:'wwz 3',
-    description:'Borderlands 3 è un videogioco action RPG-sparatutto in prima persona sviluppato da Gearbox Software e prodotto da 2K Games. Si tratta del quarto capitolo della serie Borderlands, preceduto da Borderlands: The Pre-Sequel.'}
+    {name:'Splatoon 2',
+    description:'Splatoon 2 è un videogioco del 2017 sviluppato da Nintendo EPD e pubblicato da Nintendo per Nintendo Switch. Sparatutto in terza persona, è il seguito di Splatoon, videogioco del 2015 per Wii U.'},
+    {name:'WWZ',
+    description:'World War Z è un videogioco sparatutto in terza persona sviluppato da Saber Interactive e pubblicato da Focus Home Interactive e Paramount Pictures. È stato distribuito il 16 aprile 2019 per Microsoft Windows, PlayStation 4 e Xbox One. Il videogioco è ispirato al film World War Z del 2013.'}
   ]
 
   constructor() {}
